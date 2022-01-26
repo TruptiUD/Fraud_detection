f0a434974451d995443a5997186874c750943dd6
TruptiUD <truptidhumal0813@gmail.com>
2022-01-26 14:34:20 +0530

    Added a function called sum

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
-print("dding data processing code")
+print("adding data processing code")
+
+a=5
+b=6
+
+def sum(a,b):
+    c= a+b
+    return a
+
+print("ADded a new func some which is not correct")
